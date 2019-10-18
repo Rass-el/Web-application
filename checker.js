7ea8ace6a3337e2e5ff3dd3380a4c0bc9ed487bc
Rassel <www.andrewcool@yandex.ru>
2019-10-18 13:06:03 +0300

    18.10

--- checker.js
+++ checker.js
@@ -2,10 +2,10 @@ function checkIfInside() {
     let x, y, r;
     let textY = document.getElementById("Y").value;
     let radioRadius = document.getElementsByName("radius");
-    let checkboxX = document.getElementsByName("coordinateX");
+    let checkboxX = document.getElementsByName("coordinateX[]");
     // get X
     for (let i = 0; i < checkboxX.length; i++) {
-        if (checkboxX[i].type == "checkbox" && checkboxX[i].checked) {
+        if (checkboxX[i].type === "checkbox" && checkboxX[i].checked) {
             x = checkboxX[i].value;
             break;
         } 
@@ -14,20 +14,26 @@ function checkIfInside() {
             return false;
         }
     }
+
     // get Y
     if (isNaN(textY)) {
         alert("Y must be number!");
+        
         return false;
     } 
     if (isEmpty(textY)) {
         alert("Choose Y coordinate!");
+        
         return false;
     }
     y = parseFloat(textY);
+
     if (y <= -3 || y >= 3) {
         alert("Y must be in (-3, 3)");
+        
         return false;
     }
+
     // get R
     for (let i = 0; i < radioRadius.length; i++) {
         if (radioRadius[i].type == "radio" && radioRadius[i].checked) {
@@ -36,27 +42,29 @@ function checkIfInside() {
         } 
         if (i == radioRadius.length - 1) {
             alert("Choose radius!");
+
             return false;
         }
     }
-
+    /*
     // check rectangle area
     if (x <= 0 && x >= -r && y <= r/2 && y >= 0) {
-        alert("Point inside!");
+        //alert("Point inside!");
         return true;
     }
     // check triangle area
     if (x >= 0 && x <= r/2 && y <= r/2 && y >= 0 && x <= -y + 2) {
-        alert("Point inside!");
+        //alert("Point inside!");
         return true;
     }
     // check circle area
     if (x >= 0 && x <= r/2 && y >= -r/2 && y <= 0 && x*x + y*y <= r*r/4) {
-        alert("Point inside!");
+        //alert("Point inside!");
         return true;
     }
-    alert("Point outside!")
-    return false;
+    */
+
+    return true;
 }
 
 function isEmpty(str) {
--- handler.php
+++ handler.php
@@ -5,67 +5,124 @@
         <link rel="stylesheet" href="styles/styles.css">
     </head>
     <body>
-        
+
+    <p class="center">
+        <img src="images/area.png" alt="Area" width="270px" height="270px">
+    </p>
     <?php
             $startTime = microtime();
-            $x = $_GET["coordinateX"];
-            $r = $_GET["radius"];
-            $y = $_GET["Y"];
 
-            if (validateX($x) && validateY($y) && validateR($r)) {
-                $x = (float)$x;
-                $y = (float)$y;
-                $r = (float)$r;
-                printf ("
-                    <p>
-                        <table border='1'>
-                            <tr>
-                                <td>X</td>
-                                <td>$x</td>
-                            </tr>
-                            <tr>
-                                <td>Y</td>
-                                <td>$y</td>
-                            </tr>
-                            <tr>
-                                <td>R</td>
-                                <td>$r</td>
-                            </tr>
-                            <tr>
-                                <td colspan='2'>
-                ");
-                                isInside($x, $y, $r);
-                printf ("
-                                </td>
-                            </tr>
-                        </table>
-                    </p>
-                ");
-                
-                
+            $x = $_GET["coordinateX"];
+            $r = htmlspecialchars($_GET["radius"]);
+            $y = htmlspecialchars($_GET["Y"]);
+    
+            for($i = 0; $i < count($x); ++$i) {
+                $x[$i] = substr(htmlspecialchars($x[$i]), 0, 16);
             }
+            $y = substr($y, 0, 16);
+            $r = substr($r, 0, 16);
+
+            for($i = 0; $i < count($x); ++$i) {
+                if ( validateX($x[$i], $i) ) {
+                    $x[$i] = (float)substr(htmlspecialchars($x[$i]), 0, 16);
+                    $valid_points[$i] = true;
+                }
+            }
+            
+            if ( validateY($y) ) {
+                $y = (float)$y;
+            } else {
+                for ($i = 0; $i < count($x); ++$i) {
+                    $valid_points[$i] = false;
+                }
+            }
+
+            if ( validateR($r) ) {
+                $r = (float)$r;
+            } else {
+                for ($i = 0; $i < count($x); ++$i) {
+                    $valid_points[$i] = false;
+                }
+            }
+            
+        
+    
+    ?>
+                <p>
+                    <table border=1px>
+                        <tr>
+                            <td>
+                                X
+                            </td>
+                            <td>
+                                <?php 
+                                    echo $_GET["coordinateX"][0];
+                                    for($i = 1; $i < count($x); ++$i) {
+                                        echo ", ";
+                                        echo $_GET["coordinateX"][$i];
+                                    }
+                                ?>
+                            </td>
+                        </tr>
+
+                        <tr>
+                            <td>
+                                Y
+                            </td>
+                            <td>
+                                <?php echo $_GET["Y"] ?>
+                            </td>
+                        </tr>
+
+                        <tr>
+                            <td>
+                                R
+                            </td>
+                            <td>
+                                <?php echo $_GET["radius"] ?>
+                            </td>
+                        </tr>
+
+                        <tr>
+                            <td colspan='2'>
+                                <?php 
+                                    for($i = 0; $i < count($x); ++$i) {
+                                        if ($valid_points[$i] == true) {
+                                            echo "for X=$x[$i]: ";
+                                            echo isInside($x[$i], $y, $r);
+                                            echo "<br>";
+                                        }
+                                    }
+                                ?>
+                            </td>
+                        </tr>
+                                
+                    </table>     
+                </p>
+        <?php
             $endTime = microtime();
                 $time = round( ($endTime - $startTime)* 1000, 5 );
-                printf ("<br> time = $time ms");
+                printf ("<br> <p class='center'>  time = $time ms </p>");
 
 
-            function validateX($x) {
-                if (!is_numeric($x) && $x != -2 && $x != -1.5 && $x != -1 && $x != -0.5 && $x != 0 && $x != 0.5 && $x != 1 && $x != 1.5 && $x != 2) {
-                    print "x is not valid!";
+            function validateX($x, $i) {
+                if (!is_numeric($x) || 
+                    ($x != -2 && $x != -1.5 && $x != -1 && $x != -0.5 && $x != 0 && $x != 0.5 && $x != 1 && $x != 1.5 && $x != 2) ) {
+                    print("x$i is not valid! \n");
                     return false;
                 }
                 return true;
             }
             function validateY($y) {
                 if (!is_numeric($y) || $y >= 3 || $y <= -3) {
-                    print "y is not valid!";
+                    echo "y is not valid! \n";
                     return false;
                 }
                 return true;
             }
             function validateR($r) {
-                if (!is_numeric($r) && $r != 1 && $r != 1.5 && $r != 2 && $r != 2.5 && $r != 3) {
-                    print "r is not valid!";
+                if ( !is_numeric($r) || ($r != 1 && $r != 1.5 && $r != 2 && $r != 2.5 && $r != 3) ) {
+                    print ("r is not valid! \n");
                     return false;
                 }
                 return true;
